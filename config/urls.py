a1dfa59af070724a31e07da4189686fda9c076ca
Hakob Arakelyan <hakoaraqelyan@gmail.com>
2018-06-30 13:38:43 +0400

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -4,7 +4,6 @@ from django.conf.urls import include, url
 from django.conf.urls.static import static
 
 urlpatterns = [
-    url('^admin/', admin.site.urls),
     url('^', include('apps.portfolio.urls'))
 ]
 if not settings.DEBUG:
